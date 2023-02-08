170a7118c1fbd278c79f52d1925642e6e0ac8c4b
Stauros Efthimiadis <efth.stavros@gmail.com>
2023-02-08 11:42:46 +0200

    Load data

--- classifier.py
+++ classifier.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
+import numpy as np
 import matplotlib
 import cv2
 import imghdr
@@ -7,7 +9,6 @@ import os
 # avoid oom errors by setting gpu memory consumption growth
 # grab all the gpus available in the machine
 gpus = tf.config.experimental.list_physical_devices('GPU')
-print(gpus)
 # for every gpu set memory growth (making tensorflow to keep the memory only to what it needs)
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
@@ -31,5 +32,22 @@ for image_class in os.listdir(data_dir):
                 os.remove(image_path)
         except Exception as e:
             print('issue with image {}'.format(image_path))
+
+# load data
+# build a dataset on the fly using tf.keras which will automatically create a dataset from our images
+data = tf.keras.utils.image_dataset_from_directory('data')
+# convert it to a numpy iterator (allows us to access our data pipeline)
+data_iterator = data.as_numpy_iterator()
+# get a batch from the iterator (allows us to access our data pipeline itself)
+batch = data_iterator.next()
+
+"""
+# visualize a batch
+fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
+for idx, img in enumerate(batch[0][:4]):
+    ax[idx].imshow(img.astype(int))
+    ax[idx].title.set_text(batch[1][idx])
+    plt.show()
+"""
 
 
